48abd577b1588d38ce1afedba3c85c441308885d
Heart <heartbrrrrr@protonmail.com>
2022-05-03 17:22:56 +0200

    First probably working edition

--- src/boka.py
+++ src/boka.py
@@ -1,18 +1,25 @@
 # import requests module
 import requests
+from datetime import datetime
 from secrets import login_details
+import time
+
 
 url = "https://schema.mau.se/"
 login = "login_do.jsp"
 bokning = "ajax/ajax_resursbokning.jsp?"
 
 op = "boka"
-datum = "22-05-03"
 id = "NI:A0301"
 typ = "RESURSER_LOKALER"
-intervall = "4"
-moment = "kvälls-plugg"
+intervaller = input("Vilka intervall vill du boka?").split(" ")[0:2]
+moment = "Plugg"
 flik = "FLIK-0017"
+
+
+def current_day_int():
+    return int(datetime.today().strftime("%d"))
+
 
 # create a session object
 s = requests.Session()
@@ -21,20 +28,27 @@ s.headers[
     "User-Agent"
 ] = "Mozilla/5.0 (Windows NT 10.0; rv:91.0) Gecko/20100101 Firefox/91.0"
 
-# make a get request
+day_when_start = current_day_int()
+while day_when_start == current_day_int():
+    time.sleep(300)
+
+
+# Login with an acount
 s.post(f"{url}{login}", params=login_details)
 
-# again make a get request
+# book the room
 parameters = {
     "op": op,
-    "datum": datum,
+    "datum": datetime.today().strftime("%y-%m-%d"),
     "id": id,
     "typ": typ,
-    "intervall": intervall,
+    "intervall": "temp",
     "moment": moment,
     "flik": flik,
 }
-r = s.get(f"{url}{bokning}", params=parameters)
+for intervall in intervaller:
+    parameters["intervall"] = intervall
+    r = s.get(f"{url}{bokning}", params=parameters)
 
-# check if cookie is still set
+# Print response
 print(r.text)
---
+++ src/time.py
@@ -0,0 +1,7 @@
+from datetime import datetime
+
+
+time = int(datetime.today().strftime("%d")) + 1
+
+
+print(time)
