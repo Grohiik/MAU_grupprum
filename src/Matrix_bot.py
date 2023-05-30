1c9311bea4e12c01c803efd1dd768061085a6787
Linnéa <heartbrrrrr@protonmail.com>
2023-05-30 19:03:59 +0200

    Extract the bot code from the booking code

---
+++ src/Matrix_bot.py
@@ -0,0 +1,45 @@
+import simplematrixbotlib as botlib
+import threading
+
+from boka import booking_agent, wait_till_midnight_callback
+from env import matrix_bot
+
+creds = botlib.Creds(
+    matrix_bot["homeserver"],
+    matrix_bot["username"],
+    matrix_bot["password"],
+)
+bot = botlib.Bot(creds)
+PREFIX = "!"
+
+
+# TODO Implement cancel last booking function (after callback)
+# TODO Implement help text for both cancel and booking
+# TODO Input validation
+@bot.listener.on_message_event
+async def message(matrix_room, message):
+    match = botlib.MessageMatch(matrix_room, message, bot, PREFIX)
+
+    if match.is_not_from_this_bot() and match.prefix() and match.command("book"):
+        # Write accepting message
+        await bot.api.send_text_message(
+            matrix_room.room_id,
+            "Då Bokar jag vid intervall "
+            + " och ".join(", ".join(match.args()).rsplit(", ", 1)),
+        )
+        callback = lambda: booking_agent(match)
+        thread = threading.Thread(target=wait_till_midnight_callback, args=(callback,))
+        thread.start()
+
+    if match.is_not_from_this_bot() and match.prefix() and match.command("echo"):
+        await bot.api.send_text_message(
+            matrix_room.room_id, " ".join(arg for arg in match.args())
+        )
+
+
+def main():
+    bot.run()
+
+
+if __name__ == "__main__":
+    main()
--- src/boka.py
+++ src/boka.py
@@ -1,20 +1,11 @@
 from booker import book_room
 from ics import Calendar, Event
-import simplematrixbotlib as botlib
 import time
 from datetime import datetime
 import pytz
 from env import login_details
-from env import matrix_bot
-import threading
 import asyncio
 
-
-creds = botlib.Creds(
-    matrix_bot["homeserver"], matrix_bot["username"], matrix_bot["password"]
-)
-bot = botlib.Bot(creds)
-PREFIX = "!"
 
 rooms = [
     "NI:C0401",
@@ -134,36 +125,3 @@ async def booking_agent(match):
     # Write the new calender to file
     with open("calender/my.ics", "w") as f:
         f.write(str(c))
-
-
-# TODO Implement cancel last booking function (after callback)
-# TODO Implement help text for both cancel and booking
-# TODO Input validation
-@bot.listener.on_message_event
-async def message(matrix_room, message):
-    match = botlib.MessageMatch(matrix_room, message, bot, PREFIX)
-
-    if match.is_not_from_this_bot() and match.prefix() and match.command("book"):
-        # Write accepting message
-        await bot.api.send_text_message(
-            matrix_room.room_id,
-            "Då Bokar jag vid intervall "
-            + " och ".join(", ".join(match.args()).rsplit(", ", 1)),
-        )
-        callback = lambda: booking_agent(match)
-        thread = threading.Thread(target=wait_till_midnight_callback, args=(callback,))
-        thread.start()
-
-
-    if match.is_not_from_this_bot() and match.prefix() and match.command("echo"):
-        await bot.api.send_text_message(
-            matrix_room.room_id, " ".join(arg for arg in match.args())
-            )
-
-
-def main():
-    bot.run()
-
-
-if __name__ == "__main__":
-    main()
